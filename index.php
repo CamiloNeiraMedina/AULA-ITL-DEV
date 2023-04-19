4901c35986feb7b97ee249b95a00ef802b38bd9d
CamiloNeiraMedina <86502849+CamiloNeiraMedina@users.noreply.github.com>
2023-04-18 20:47:22 -0600

    Boceto
    Boceto del proyecto

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <?php include("pages/head.php")?>
+</head>
+<body>
+    <div class="invitacion">
+        <h1>¿Cuenta ya con algun accesos?</h1>
+        <button class="log-in" type="button">Entrar</button>
+        <button class="sign-in" type="button">Registrarse</button>
+        
+    </div>
+    
+</body>
+</html>
---
+++ pages/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/style.css">
+    
+<body>
+    <div class="encabezado cuadros-100">
+        <h1>Bienvenido a tu Aula Virtual</h1>
+        <h2>Este es una prueba de aula</h2>
+    
+    </div>
+</body>
+</html>
